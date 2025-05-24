2d93e55d0fbd564a58caf35838fafc757b260982
Vaibhav Kharate <vaibhav.s.kharate@gmail.com>
2025-05-24 23:32:25 +0530

    mongoose module setup

---
+++ MongoDB/index.js
@@ -0,0 +1,34 @@
+const express = require ("express");
+const app = express ();
+const mongoose = require ("mongoose");
+const path = require ("path");
+const Chat = require ("./models/chat.js")
+
+main()
+.then(() => {
+    console.log("connection successful");  
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+}
+
+let chat1 = new Chat({
+    from: "vaibhav",
+    to:"hari",
+    msg: "the start of building",
+    created_at: new Date(),
+});
+
+chat1.save().then((res) => {
+    console.log(res);
+    
+});
+app.get("/", (req, res) => {
+    res.send("root is working")
+});
+app.listen(8080, () => {
+    console.log("server is listening on port 8080"); 
+});
